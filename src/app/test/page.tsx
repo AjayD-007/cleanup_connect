b46e17060265182d7dc7001bad484e01ea054acf
Ajay D <84711054+AjayD-007@users.noreply.github.com>
2024-03-19 18:34:29 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,44 +1,20 @@
 'use client'
 import Script from "next/script";
-import { useEffect, useState } from "react";
 
-export default function TestPage(){
-    const [ticketprixEmbed, setTicketprixEmbed] = useState<any>(null);
-  function toggleDrawer() {
-    const drawerContainer = document.querySelector(".drawer-container");
-    drawerContainer!.classList.toggle("open-drawer");
-    ticketprixEmbed!.init({
-      mountId: "ticketprix-embed",
-      eventSlug: "iplfandomcarnivalfanparks",
-      frameHeight: "100%",
-      merchantId: "5fbd3fbb24dfac00083558cd",
-    });
-  }
-  useEffect(() => {
-    let ticketprixEmbed:any; // Define ticketprixEmbed here
-    // Load the script and assign ticketprixEmbed
-    const script = document.createElement("script");
-    script.src = "https://www.ticketprix.com/api/script/embed/event";
-    script.async = true;
-    script.onload = () => {
-      setTicketprixEmbed((window as any).ticketprixEmbed); // Assign ticketprixEmbed
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []); 
+export default function Home() {
+   
   return (
     <>
-    <Script src={"https://www.ticketprix.com/api/script/embed/event"} />
-    <div className="drawer-container">
-    <div className="drawer">
-      
-      <div id="ticketprix-embed"></div>
-    </div>
-  </div>
-  <div className="drawer-toggle" onClick={toggleDrawer}>☰</div>
+    <div id="ticketprix-embed"></div>
+    <Script src={"https://www.ticketprix.com/api/script/embed/event"}
+     onLoad={()=>{(window as any).ticketprixEmbed!.init({
+      mountId: "ticketprix-embed",
+      eventSlug: "kkk",
+      frameHeight: "100%",
+      merchantId: "5fbd3fbb24dfac00083558cd",
+    });}} 
+     />
+    
     </>
 
   );
