eef89ddd48b1d19d0f59195d7574c0ab28ec1881
Ajay D <84711054+AjayD-007@users.noreply.github.com>
2024-03-21 09:53:02 +0530

    Update page.tsx

--- src/app/test/[slug]/page.tsx
+++ src/app/test/[slug]/page.tsx
@@ -1,32 +1,21 @@
 'use client'
-import { usePathname } from "next/navigation";
 import Script from "next/script";
-import { useEffect, useState } from "react";
 
-export default function Home({params:{slug}}:{params:{slug:string}}) {
-    const [url, setUrl] = useState('');
-
-    useEffect(() => {
-      if (typeof window !== 'undefined') { // Check for client-side environment
-        setUrl(window.location.href);
-      }
-    }, []);
+export default function Home({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
   return (
     <>
-    <div id="ticketprix-embed"></div>
-    {url !=""&&<Script src={"https://www.ticketprix.com/api/script/embed/event"}
-     onLoad={()=>{(window as any).ticketprixEmbed!.init({
-      mountId: "ticketprix-embed",
-      eventSlug: slug,
-      frameHeight: "100%",
-      url:url,
-      merchantId: "5fbd3fbb24dfac00083558cd",
-    });}} 
-     />}
-    
+      <div id="ticketprix-embed"></div>
+      <div id="ticketprix-url-embed"></div>
+      <Script
+        src={"https://www.ticketprix.com/api/script/embed/event"}
+        onLoad={() => {
+          (window as any).initTicketprixBooking({ eventSlug: slug });
+        }}
+      />
     </>
-
   );
 }
-
-// {params:{slug}}:{params:{slug:string}}
